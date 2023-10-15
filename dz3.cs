10383e0b091b01fdc7edd0fc0eb48a561fcd808a
DenisGrabovskiy <146859255+DenisGrabovskiy@users.noreply.github.com>
2023-10-15 23:40:13 +0300

    Add files via upload

---
+++ dz3.cs
@@ -0,0 +1,38 @@
+﻿int ReadInt(string message) // Ввод переменных
+{
+    System.Console.WriteLine(message);// Получаем данные из Строки
+    string value = Console.ReadLine();// Вызов консольного ввода
+    return int.Parse(value);// Возвращает переменную в int
+}
+
+bool Weekend(int weekday)
+{
+if (weekday>5)
+{
+    return true;
+}
+return false;
+}
+
+bool ValidateWeekday(int number)
+{
+if (number>0 && number <=7)
+{
+    return true;
+}
+Console.WriteLine("Это не день недели");
+return false;
+}
+
+int weekday= ReadInt("Введите день недели");
+if (ValidateWeekday(weekday))
+{
+    if (Weekend(weekday))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else 
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
